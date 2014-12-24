913af4ee9edbdefd16ddeb9bb138ad086cd58ab6
Francesc Guasch <frankie@etsetb.upc.edu>
2014-12-24 11:49:33 +0100

    works for me

--- cbc.pl
+++ cbc.pl
@@ -3,32 +3,40 @@
 use warnings;
 use strict;
 
+use eBD::Process;
+
 use Crypt::CBC;
 use Crypt::Mode::CBC;
+use Data::Dumper;
 use Digest::HMAC;
 use Digest::SHA qw(hmac_sha256_hex);
 
 # key length has to be valid key size for this cipher
+my $TABLE = 'compra';
+my $FIELD_SRC = 'cadena';
+my $FIELD_DST = 'encrypt';
+
 my $KEY =  '55a51621a6648525';
 
-# això m'ho invento, necessitem el iv
 my  $IV = $KEY;
+my $LOG;
 
-my $CADENA ='VendorTxCode=TxCode-1310917599-223087284&Amount=36.95&Currency=GBP&Description=description&CustomerName=Fname Surname&CustomerEMail=customer@example.com&BillingSurname=Surname&BillingFirstnames=Fname&BillingAddress1=BillAddress Line 1&BillingCity=BillCity&BillingPostCode=W1A1BL&BillingCountry=GB&BillingPhone=447933000000&DeliveryFirstnames=Fname&DeliverySurname=Surname&DeliveryAddress1=BillAddress Line 1&DeliveryCity=BillCity&DeliveryPostCode=W1A1BL&DeliveryCountry=GB&DeliveryPhone=447933000000&SuccessURL=https://example.com/success&FailureURL=https://example.co/failure';
+my $FILE_LOG = $0;
+$FILE_LOG =~ s{.*/}{};
+$FILE_LOG = "/var/tmp/$FILE_LOG.log";
 
-my $FILE_EXPECTED = 'expected.txt';
-my ($EXPECTED,$EXPECTED_START);
-
-###################################################################################
+########################################################################
 
 sub crypt_mode {
+    my $cadena = shift;
     print "Crypt::Mode::CBC\n";
     my $cipher = Crypt::Mode::CBC->new('AES');
-    my $crypt = $cipher->encrypt($CADENA, $KEY ,$IV);
+    my $crypt = $cipher->encrypt($cadena, $KEY ,$IV);
     check_output( bin2hex($crypt));
 }
 
 sub crypt_cbc {
+    my $cadena = shift;
     print "Crypt::CBC\n";
     my $cipher = Crypt::CBC->new( -key    => $KEY
         , -iv => $KEY
@@ -37,9 +45,8 @@ sub crypt_cbc {
         , -header => 'none'
         , -keysize => 16
     );
-    my $encrypted = $cipher->encrypt($CADENA);
-#    my $base64 = encode_base64($encrypted);
-    check_output(bin2hex($encrypted));
+    my $encrypted = $cipher->encrypt($cadena);
+    return bin2hex($encrypted);
 }
 
 
@@ -53,45 +60,43 @@ sub bin2hex {
     return $out;
 }
 
-sub init_expected_text {
-    open my $in,'<',$FILE_EXPECTED or die "$! $FILE_EXPECTED";
-
-    $EXPECTED ='';
-    while (<$in>) {
-        chomp;
-        $EXPECTED .= $_;
+sub select_cadena {
+    my ($dbh, $id) = @_;
+    my $sth = $dbh->prepare(
+        "SELECT _idRegistro,$FIELD_SRC FROM $TABLE "
+        ." WHERE _idRegistro=?"
+    );
+    $sth->execute($id);
+    my ($id_registro, $cadena) = $sth->fetchrow;
+    $sth->finish;
+    if (!$id_registro) {
+        my $msg = "ERROR: No existe registro $id en tabla $TABLE";
+        print_log($msg);
+        die $msg;
     }
-    ($EXPECTED_START) = $EXPECTED =~ m{(^.{10})};
+
+    if ( !$cadena ) {
+        my $msg = "WARNING: Cadena vacia id=$id en tabla $TABLE";
+        print_log($msg);
+        warn($msg);
+    }
 }
 
-sub check_output {
-    my $text = shift;
-    if ($text =~ /^$EXPECTED_START/) {
-        print "Té bona pinta, comença igual\n";
-        if ($text eq $EXPECTED) {
-            print "OK tot.\n" 
-        } else {
-            print "ERROR. No coincideix\n";
-            my $min_length = length($text);
-            $min_length = length($EXPECTED) if length($EXPECTED)<length($text);
-            my $cont;
-            for ($cont=0;$cont<=$min_length;$cont++) {
-                print substr($text,$cont,1);
-                last if substr($text,$cont,1) ne substr($EXPECTED,$cont,1);
-            }
-            print "\n";
-            print "Són iguals fins al caràcter $cont de ".length($EXPECTED)."\n";
-            print "text:\n$text\nesperat:\n$EXPECTED\n";
-        }
-    } else {
-        $text = substr($text,0,30)." ... ";
-        print "ERROR. Hauria de començar per $EXPECTED_START\n$text\n";
-    }
-    print "\n";
+sub encrypt_cadena {
+    my $dbh = open_dbh();
+    my $id = $ARGV[0];
+    my $cadena = select_cadena($dbh,$id);
+    my $encrypt = crypt_cbc($cadena);
+    my $sth = $dbh->prepare(
+        "UPDATE $TABLE "
+        ." SET $FIELD_DST=? "
+        ." WHERE _idRegistro=? "
+    );
+    $sth->execute($encrypt, $id);
+    $sth->finish;
 }
-
 ####################################################
 
-init_expected_text();
-crypt_mode();
-crypt_cbc();
+#log_env();
+print_log(" argv = ".join(" , ",@ARGV));
+encrypt_cadena();
